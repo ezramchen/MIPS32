c5b709db4e2e7aa37cdba08b4512ffcb8044e919
ezramchen <ezra.chen@sjsu.edu>
2026-04-05 02:09:59 -0700

    Create IM.v

---
+++ NoFwd/IM.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+// Company: SJSU
+// Engineer: Ezra Chen
+// Create Date: 04/04/2026 07:33:15 PM
+// Module Name: IM
+// Project Name: MIPS32
+//////////////////////////////////////////////////////////////////////////////////
+
+module IM(
+    input [31:0] addr, // address from PC
+    output [31:0] instr // instruction at that address
+);
+
+    // declare memory
+    reg [31:0] mem[0:1023]; // 4KB of memory (1024 words)
+
+    // configure instructions
+    assign instr = mem[addr[11:2]]; // word-aligned address, ignore byte offset
+
+    // load instructions
+    initial begin 
+        $readmemh("program.mem", mem); // load instructions from hex file
+    end
+endmodule
